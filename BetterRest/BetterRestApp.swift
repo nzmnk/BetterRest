2e7fe900638f8b0b419262cbe6d25cbf3e7d10e0
Nikita Zimenko <nic.zimen@gmail.com>
2021-04-13 20:51:23 +0300

    Initial Commit
    Just initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Никита Зименко on 13.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
