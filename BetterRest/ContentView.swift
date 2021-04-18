bc56331bc34f27c12c66a6a95af963336b411ca7
Nikita Zimenko <nic.zimen@gmail.com>
2021-04-18 20:26:39 +0300

    Complete working app
    Tasks remaining: 1. Delete button; 2. Replace stepper in coffeee consumption check with picker.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,134 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        NavigationView {
+            Form {
+                Section(header: Text("Waking Time")) {  //Section layout
+//                    Text("Set the Waking Time")
+//                        .font(.headline)
+                   
+                    
+                    DatePicker("Choose time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+                
+                
+//                VStack (alignment: .leading, spacing: 0) {    //VStack layout
+//                    Text("Waking Time")
+//                        .font(.headline)
+//
+//
+//                    DatePicker("Choose time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+//                        .labelsHidden()
+//                        .datePickerStyle(WheelDatePickerStyle())
+//                }
+                
+                
+                Section (header: Text("Sleeping Time")) {   //Section layout
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                
+//                VStack (alignment: .leading, spacing: 0) {   //VStack layout
+//                    Text("Sleeping Time")
+//                        .font(.headline)
+//
+//
+//                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+//                        Text("\(sleepAmount, specifier: "%g") hours")
+//                    }
+//                }
+                
+                
+                Section(header: Text("Daily coffee intake")) {  //Section layout
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 Cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                }
+                
+                
+//                VStack (alignment: .leading, spacing: 0) {    //VStack layout
+//                    Text("Daily coffee intake")
+//                        .font(.headline)
+//
+//
+//                    Stepper(value: $coffeeAmount, in: 1...20) {
+//                        if coffeeAmount == 1 {
+//                            Text("1 Cup")
+//                        } else {
+//                            Text("\(coffeeAmount) cups")
+//                        }
+//                    }
+//                }
+            }
             .padding()
+            
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+            
+            
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            })
+        }
+    }
+    
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
+    
+    func calculateBedtime() {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "You'd better go to bed at"
+            
+        } catch  {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
     }
 }
 
